f8b10a306ed75c508b972170ead3d85a3df7c282
Aria rouhani <aria.rouhan@gmail.com>
2021-03-04 13:13:57 +0330

    Finished!

--- soursecode/AI/ai.py
+++ soursecode/AI/ai.py
@@ -9,3 +9,4 @@ def fill_spot(l1: Letters, b1: Board):
 
     for i in range(len(b1.spot)):
         b1.spot[i] = l1.alphabet[random.randrange(0, len(l1.alphabet))]
+
--- soursecode/Main.py
+++ soursecode/Main.py
@@ -9,4 +9,56 @@ b1 = Board()
 l1 = Letters()
 
 ai.fill_spot(l1, b1)
-print(b1.spot)  # TODO remove the debug line
+
+
+
+def game_over(state: bool):
+    if state:
+        print("***YOU WIN ASSHOLE***")
+    else:
+        print('sry you are out of lives! Fucking bitch')
+
+
+def correctness(guess: str):
+    temp = []
+    for i in range(len(b1.spot)):
+        if guess[i] == b1.spot[i]:
+            temp.append("C")
+        else:
+            if guess[i] in b1.spot:
+                temp.append("MP")
+            else:
+                temp.append("N")
+
+    return temp
+
+
+def handle_lives(guess, lives):
+    temp = correctness(guess)
+    print()
+    print(list(guess))
+    print(temp)
+    print()
+    for i in range(len(temp)):
+        if temp[i] != "C":
+            lives -= 1
+            return lives
+    else:
+        game_over(True)
+
+
+def init():
+    lives = len(b1.spot) * 3
+    while lives > 0:
+        print("You have", lives, "left")
+        guess = input('please enter your guess : ').upper()
+        if guess == "HESOYAM":
+            print(b1.spot)
+        lives = handle_lives(guess, lives)
+        if lives is None:
+            break
+    if lives == 0:
+        game_over(False)
+
+
+init()
